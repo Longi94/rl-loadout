8fa7329ae80d417c442208e90461f643df8666a9
Long Tran <lngtrn94@gmail.com>
2019-07-16 02:27:05 +0200

    init threeejs

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CanvasComponent } from './components/canvas/canvas.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CanvasComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/components/canvas/canvas.component.ts
@@ -0,0 +1,55 @@
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import * as THREE from 'three';
+import { animate } from "@angular/animations";
+
+@Component({
+  selector: 'app-canvas',
+  templateUrl: './canvas.component.html',
+  styleUrls: ['./canvas.component.scss']
+})
+export class CanvasComponent implements OnInit {
+
+  @ViewChild('canvas', {static: true})
+  canvas: ElementRef;
+
+  private camera;
+  private scene;
+  private geometry;
+  private material;
+  private mesh;
+  private renderer;
+
+
+  constructor() {
+  }
+
+  ngOnInit() {
+    this.camera = new THREE.PerspectiveCamera(70, window.innerWidth / window.innerHeight, 0.01, 10);
+    this.camera.position.z = 1;
+
+    this.scene = new THREE.Scene();
+
+    this.geometry = new THREE.BoxGeometry(0.2, 0.2, 0.2);
+    this.material = new THREE.MeshNormalMaterial();
+
+    this.mesh = new THREE.Mesh(this.geometry, this.material);
+    this.scene.add(this.mesh);
+
+    this.renderer = new THREE.WebGLRenderer({canvas: this.canvas.nativeElement, antialias: true});
+    this.renderer.setSize(window.innerWidth, window.innerHeight);
+    document.body.appendChild(this.renderer.domElement);
+
+    this.animate();
+  }
+
+  animate() {
+
+    requestAnimationFrame(() => this.animate());
+
+    this.mesh.rotation.x += 0.01;
+    this.mesh.rotation.y += 0.02;
+
+    this.renderer.render(this.scene, this.camera);
+
+  }
+}
