3980ef4472a586020c24a5877b8dda639d0dbc40
Long Tran <lngtrn94@gmail.com>
2019-07-17 12:36:09 +0200

    check for blue value

--- src/app/components/skin/static-skin.ts
+++ src/app/components/skin/static-skin.ts
@@ -22,7 +22,8 @@ export class StaticSkin {
   private update() {
     for (let i = 0; i < this.data.length; i += 4) {
       let color = new Color(0, 0, 0);
-      if (this.rgbaMap[i] == 255) {
+
+      if (this.rgbaMap[i] == 255 && this.rgbaMap[i + 2] == 0) {
         color = this.primary;
 
         if (this.rgbaMap[i + 3] > 0) {
@@ -32,12 +33,12 @@ export class StaticSkin {
         if (this.rgbaMap[i + 1] > 0) {
           color = overBlendColors(this.paint, this.primary, this.rgbaMap[i + 3]);
         }
-
-        this.data[i] = color.r * 255;
-        this.data[i + 1] = color.g * 255;
-        this.data[i + 2] = color.b * 255;
-        this.data[i + 3] = 255;
       }
+
+      this.data[i] = color.r * 255;
+      this.data[i + 1] = color.g * 255;
+      this.data[i + 2] = color.b * 255;
+      this.data[i + 3] = 255;
     }
   }
 
