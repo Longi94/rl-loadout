e65745cfce6cd69a00f1f65b644b05f239edcc9d
Long Tran <lngtrn94@gmail.com>
2019-07-21 00:03:28 +0200

    fix texture size

--- src/app/3d/rgba-map-pipe-texture.ts
+++ src/app/3d/rgba-map-pipe-texture.ts
@@ -1,9 +1,6 @@
 import { TgaRgbaLoader } from "../utils/tga-rgba-loader";
 import { Color, DataTexture, RGBAFormat } from "three";
 
-const WIDTH = 2048;
-const HEIGHT = 2048;
-
 export abstract class RgbaMapPipeTexture {
   baseUrl: string;
   rgbaMapUrl: string;
@@ -13,14 +10,13 @@ export abstract class RgbaMapPipeTexture {
   rgbaMap: Uint8ClampedArray;
   data: Uint8Array;
 
+  width: number = 2048;
+  height: number = 2048;
   texture: DataTexture;
 
   protected constructor(baseUrl, rgbaMapUrl) {
     this.baseUrl = baseUrl;
     this.rgbaMapUrl = rgbaMapUrl;
-
-    this.data = new Uint8Array(WIDTH * HEIGHT * 4);
-    this.texture = new DataTexture(this.data, WIDTH, HEIGHT, RGBAFormat);
   }
 
   /**
@@ -33,7 +29,7 @@ export abstract class RgbaMapPipeTexture {
       promises.push(new Promise((resolve, reject) => {
         this.loader.load(this.baseUrl, buffer => {
           this.base = buffer.data;
-          resolve();
+          resolve(buffer);
         }, undefined, reject);
       }));
     }
@@ -42,12 +38,23 @@ export abstract class RgbaMapPipeTexture {
       promises.push(new Promise((resolve, reject) => {
         this.loader.load(this.rgbaMapUrl, buffer => {
           this.rgbaMap = buffer.data;
-          resolve();
+          resolve(buffer);
         }, undefined, reject);
       }));
     }
 
-    return Promise.all(promises);
+    return new Promise((resolve, reject) => {
+      Promise.all(promises).then(values => {
+        if (values.length > 0) {
+          this.width = values[0].width;
+          this.height = values[0].height;
+        }
+
+        this.data = new Uint8Array(this.width * this.height * 4);
+        this.texture = new DataTexture(this.data, this.width, this.height, RGBAFormat);
+        resolve();
+      }, reject);
+    });
   }
 
   /**
