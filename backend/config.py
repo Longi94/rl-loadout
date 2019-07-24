efc654f94611f2542a460307277db5b5b62ea703
Long Tran <lngtrn94@gmail.com>
2019-07-24 21:47:30 +0200

    read config from etc

--- backend/config.py
+++ backend/config.py
@@ -4,11 +4,13 @@ import configparser
 
 log = logging.getLogger(__name__)
 
+ETC_CONFIG = '/etc/rl-loadout/config.ini'
 CONFIG_FILE = 'config.ini'
 
 config = configparser.ConfigParser()
 
+if os.path.exists(ETC_CONFIG):
+    config.read(ETC_CONFIG)
+
 if os.path.exists(CONFIG_FILE):
     config.read(CONFIG_FILE)
-else:
-    log.warning('Config file not found, using defaults.')
