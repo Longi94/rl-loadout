ba06f6e00a5431fa9d935f71868ba8ad1ad5e71c
Long Tran <lngtrn94@gmail.com>
2019-07-24 11:31:11 +0200

    fix empty string assets

--- src/app/utils/network.ts
+++ src/app/utils/network.ts
@@ -1,7 +1,7 @@
 import { environment } from "../../environments/environment";
 
 export function getAssetUrl(asset: string) {
-  if (asset == undefined) {
+  if (asset == undefined || asset.length == 0) {
     return undefined;
   }
   return `${environment.assetHost}/${asset}`;
