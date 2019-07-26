7f6a0944a2f8de71295f8245ba9fa63a6d0e1bfe
Long Tran <lngtrn94@gmail.com>
2019-07-26 02:20:15 +0200

    fix prod version

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   // @ts-ignore
-  version: require('../../package.json').version + '-DEV',
+  version: require('../../package.json').version,
   production: true,
   assetHost: 'https://storage.googleapis.com/rl-loadout',
   backend: 'https://rocket-loadout.com'
