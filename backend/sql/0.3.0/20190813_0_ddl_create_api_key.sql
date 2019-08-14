d7932c88efdce31c5e9eee0979b656630e27a5b1
Long Tran <lngtrn94@gmail.com>
2019-08-14 13:11:49 +0200

    update sql

--- backend/sql/0.3.0/20190813_0_ddl_create_api_key.sql
+++ backend/sql/0.3.0/20190813_0_ddl_create_api_key.sql
@@ -1,8 +1,10 @@
 CREATE TABLE api_key
 (
+    id          SERIAL       NOT NULL,
     key         VARCHAR(32)  NOT NULL,
     name        VARCHAR(255) NOT NULL,
     description VARCHAR(255) NOT NULL,
     active      BOOLEAN      NOT NULL,
-    PRIMARY KEY (key)
+    PRIMARY KEY (id),
+    UNIQUE (key)
 );
