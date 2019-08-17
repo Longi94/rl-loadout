f2fba5c8aa953fa44a9041dec57dd7fa3238d508
Long Tran <lngtrn94@gmail.com>
2019-08-18 00:17:14 +0200

    add hitbox input

--- frontend/src/app/admin/components/dialog/create-dialog.ts
+++ frontend/src/app/admin/components/dialog/create-dialog.ts
@@ -2,6 +2,7 @@ import { OnInit } from "@angular/core";
 import { Quality } from "../../../model/quality";
 import { CloudObject, CloudStorageService } from "../../../service/cloud-storage.service";
 import { MatDialogRef } from "@angular/material";
+import { Hitbox } from "../../../model/hitbox";
 
 export abstract class CreateDialog implements OnInit {
 
@@ -15,6 +16,15 @@ export abstract class CreateDialog implements OnInit {
     {value: Quality.BLACK_MARKET, name: 'Black market'},
     {value: Quality.LIMITED, name: 'Limited'},
     {value: Quality.PREMIUM, name: 'Premium'}
+  ];
+
+  hitboxes = [
+    {value: Hitbox.OCTANE, name: 'Octane'},
+    {value: Hitbox.DOMINUS, name: 'Dominus'},
+    {value: Hitbox.PLANK, name: 'Plank'},
+    {value: Hitbox.BREAKOUT, name: 'Breakout'},
+    {value: Hitbox.HYBRID, name: 'Hybrid'},
+    {value: Hitbox.BATMOBILE, name: 'Batmobile'}
   ];
 
   objects: { [key: string]: CloudObject[] } = {
