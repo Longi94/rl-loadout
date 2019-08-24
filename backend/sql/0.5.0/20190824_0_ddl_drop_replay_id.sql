119f4c874956a9f85183aec8bf01e7bde279199a
Long Tran <lngtrn94@gmail.com>
2019-08-24 20:20:32 +0200

    drop replay_id, use id as item id

---
+++ backend/sql/0.5.0/20190824_0_ddl_drop_replay_id.sql
@@ -0,0 +1,5 @@
+ALTER TABLE public.antenna DROP COLUMN replay_id;
+ALTER TABLE public.body DROP COLUMN replay_id;
+ALTER TABLE public.decal_detail DROP COLUMN replay_id;
+ALTER TABLE public.topper DROP COLUMN replay_id;
+ALTER TABLE public.wheel DROP COLUMN replay_id;
