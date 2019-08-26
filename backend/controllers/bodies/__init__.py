149bed00f27761f628b8c5353b0f9138f996c5c5
Long Tran <lngtrn94@gmail.com>
2019-08-26 16:07:08 +0200

    rest get body by id

--- backend/controllers/bodies/__init__.py
+++ backend/controllers/bodies/__init__.py
@@ -3,7 +3,7 @@ from flask import jsonify, request
 from config import config
 from entity import Body
 from dao import BodyDao
-from utils.network.exc import UnauthorizedException
+from utils.network.exc import UnauthorizedException, NotFoundException
 from rocket.ids import *
 
 body_dao = BodyDao()
@@ -15,14 +15,26 @@ def get(id=None):
     if api_key is None:
         raise UnauthorizedException('API key not provided')
 
-    if id is None:
-        bodies = body_dao.get_all()
-    else:
-        body = body_dao.get(id)
-        bodies = [] if body is None else [body]
+    bodies = body_dao.get_all()
 
     bodies = list(map(lambda x: _to_body_response(x, api_key), bodies))
     return jsonify(bodies), 200
+
+
+def get_by_id(id: int):
+    """
+    :param id: ID of the item (in-game item ID).
+    """
+    api_key = request.args.get('key', None)
+    if api_key is None:
+        raise UnauthorizedException('API key not provided')
+
+    body = body_dao.get(id)
+
+    if body is None:
+        raise NotFoundException('Body not found')
+
+    return jsonify(_to_body_response(body, api_key)), 200
 
 
 def _to_body_response(body: Body, api_key: str) -> Dict[str, Any]:
