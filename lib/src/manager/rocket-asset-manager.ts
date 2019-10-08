b9d15749e0bd26e11b1e5e042d92f4aede5e8a44
Long Tran <lngtrn94@gmail.com>
2019-10-09 00:47:03 +0200

    fallback models

--- lib/src/manager/rocket-asset-manager.ts
+++ lib/src/manager/rocket-asset-manager.ts
@@ -6,6 +6,8 @@ import { PaintConfig } from '../model/paint-config';
 import { WheelsModel } from '../3d/wheels-model';
 import { RocketConfig } from '../model/rocket-config';
 import { DRACOLoader } from 'three/examples/jsm/loaders/DRACOLoader';
+import { Body } from '../model/body';
+import { Wheel } from '../model/wheel';
 
 export class RocketAssetManager {
   private readonly rlService: RocketLoadoutService;
@@ -28,8 +30,16 @@ export class RocketAssetManager {
     this.rlService = new RocketLoadoutService(config.backendHost);
   }
 
-  async loadBody(id: number, paintConfig: PaintConfig): Promise<BodyModel> {
-    const body = await this.rlService.getBody(id);
+  async loadBody(id: number, paintConfig: PaintConfig, fallback?: number): Promise<BodyModel> {
+    let body: Body;
+
+    try {
+      body = await this.rlService.getBody(id);
+    } catch (e) {
+      if (fallback != undefined) {
+        body = await this.rlService.getBody(fallback);
+      }
+    }
 
     const bodyModel = new BodyModel(body, Decal.NONE, paintConfig, this.config);
     await bodyModel.load();
@@ -37,8 +47,16 @@ export class RocketAssetManager {
     return bodyModel;
   }
 
-  async loadWheel(id: number, paintConfig: PaintConfig): Promise<WheelsModel> {
-    const wheel = await this.rlService.getWheel(id);
+  async loadWheel(id: number, paintConfig: PaintConfig, fallback?: number): Promise<WheelsModel> {
+    let wheel: Wheel;
+
+    try {
+      wheel = await this.rlService.getWheel(id);
+    } catch (e) {
+      if (fallback != undefined) {
+        wheel = await this.rlService.getWheel(fallback);
+      }
+    }
 
     const wheelsModel = new WheelsModel(wheel, paintConfig, this.config);
     await wheelsModel.load();
--- lib/src/utils/network.ts
+++ lib/src/utils/network.ts
@@ -3,6 +3,14 @@ import { RocketConfig } from '../model/rocket-config';
 export async function doRequest<T>(request: RequestInfo | string): Promise<T> {
     const response = await fetch(request);
     const json = await response.json();
+
+    if (!response.ok) {
+        throw {
+            status: response.status,
+            body: json
+        }
+    }
+
     return json as T;
 }
 
