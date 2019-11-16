2a139f292b6d95d3c8a74f5ab003ec98e8109c50
Long Tran <lngtrn94@gmail.com>
2019-11-16 19:23:17 +0100

    update thumbnail converter

--- tools/convert_thumbnails.py
+++ tools/convert_thumbnails.py
@@ -3,41 +3,31 @@
 import argparse
 import os
 from PIL import Image
-from subprocess import Popen, PIPE
 
 
-def umodel_extract(path, file):
-    print(f'Extracting {file}...')
-    command = ['umodel', '-game=rocketleague', f'-path={path}', '-export', file]
-    print(f'Running {" ".join(command)}')
-    p = Popen(command, stdout=PIPE)
-    for line in p.stdout:
-        print(line.decode("utf-8"), end='')
-    p.wait()
-
-    if p.returncode != 0:
-        print(f'umodel returned with non-zero exit code: {p.returncode}')
-
-
-def convert_to_jpeg(path, file):
-    print(f'Converting {os.path.join(path, file)}...')
-    im = Image.open(os.path.join(path, file))
+def convert_to_jpeg(file, output):
+    print(f'Converting {file}...')
+    im = Image.open(file)
     rgb_im = im.convert('RGB')
-    rgb_im.save(os.path.join('thumbnails', file.replace('.tga', '.jpg')))
+    rgb_im.save(output)
 
 
 parser = argparse.ArgumentParser()
-parser.add_argument('--input', '-i', type=str, help='Path to game files', required=True)
+parser.add_argument('-d', '--directory', type=str, required=True)
+parser.add_argument('-o', '--output', type=str, required=True)
 args = parser.parse_args()
 
-files = os.listdir(args.input)
+os.makedirs(args.output)
 
-for file in filter(lambda x: x.endswith('_T_SF.upk'), files):
-    umodel_extract(args.input, file)
+for directory in os.listdir(args.directory):
+    directory = os.path.join(args.directory, directory)
+    if not os.path.isdir(directory):
+        continue
 
-os.mkdir('thumbnails')
+    if not directory.endswith('_T_SF'):
+        continue
 
-for root, subdirs, files in os.walk('UmodelExport'):
-    for file in files:
-        if file.lower().endswith('thumbnail.tga'):
-            convert_to_jpeg(root, file)
+    for file in os.listdir(os.path.join(directory, 'Texture2D')):
+        if file.endswith('.tga'):
+            convert_to_jpeg(os.path.join(directory, 'Texture2D', file),
+                            os.path.join(args.output, file.replace('.tga', '.jpg')))
